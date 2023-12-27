b15c39808711ac020a401bd7800cf4a45bbf9715
Ali <ali.rezai27461@gmail.com>
2023-12-27 09:00:41 +0100

    22

--- src/ts/functions.ts
+++ src/ts/functions.ts
@@ -1,7 +1,9 @@
-
-export function toggleHamburger () {
-    const navbar = document.getElementById ("navbar");
+export function toggleHamburger() {
+  const navbar = document.getElementById("navbar");
+  const toggleTest = () => {
     navbar?.classList.toggle("hamburger-on");
     navbar?.classList.toggle("hamburger-off");
-    
+  };
+
+  toggleTest();
 }

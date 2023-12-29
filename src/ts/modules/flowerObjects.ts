eedf8fc8f1700e1c16c11bda13cd4900130fafe5
Matilda <Matilda.Wallin@medieinstitutet.se>
2023-12-29 15:57:35 +0100

    added flowerpictures

--- src/ts/modules/flowerObjects.ts
+++ src/ts/modules/flowerObjects.ts
@@ -1,10 +1,10 @@
 import { Flowers } from "./flowersClass";
 
-export const flower1 = new Flowers ("https://i.pinimg.com/564x/71/8f/08/718f08b6ddc5768e36341d70fdf0f822.jpg", "Roses", "A bouquet of red roses", 199);
-export const flower2 = new Flowers ("https://i.pinimg.com/564x/71/8f/08/718f08b6ddc5768e36341d70fdf0f822.jpg", "Roses", "A bouquet of red roses", 199);
-export const flower3 = new Flowers ("https://i.pinimg.com/564x/71/8f/08/718f08b6ddc5768e36341d70fdf0f822.jpg", "Roses", "A bouquet of red roses", 199);
-export const flower4 = new Flowers ("https://i.pinimg.com/564x/71/8f/08/718f08b6ddc5768e36341d70fdf0f822.jpg", "Roses2", "A bouquet of red roses", 199);
-export const flower5 = new Flowers ("https://i.pinimg.com/564x/71/8f/08/718f08b6ddc5768e36341d70fdf0f822.jpg", "Roses2", "A bouquet of red roses", 199);
-export const flower6 = new Flowers ("https://i.pinimg.com/564x/71/8f/08/718f08b6ddc5768e36341d70fdf0f822.jpg", "Roses2", "A bouquet of red roses", 199);
+export const flower1 = new Flowers ("/src/img/flower1.jpg", "Roses", "A bouquet of red roses", 199);
+export const flower2 = new Flowers ("/src/img/flower2.jpg", "Roses", "A bouquet of red roses", 199);
+export const flower3 = new Flowers ("/src/img/flower3.jpg", "Roses", "A bouquet of red roses", 199);
+export const flower4 = new Flowers ("/src/img/flower4.jpg", "Roses2", "A bouquet of red roses", 199);
+export const flower5 = new Flowers ("/src/img/flower5.jpg", "Roses2", "A bouquet of red roses", 199);
+export const flower6 = new Flowers ("/src/img/flower1.jpg", "Roses2", "A bouquet of red roses", 199);
 
 

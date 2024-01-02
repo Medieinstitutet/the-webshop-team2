9d456d4dfb842782d004d7dc34c49d6cbc9b2208
Ali Rezai <Ali.rezai27461@gmail.com>
2024-01-02 12:59:16 +0100

    töm cart list

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -107,11 +107,15 @@ finalPaymentBtn.addEventListener ("click", () => {
     const loader = document.getElementById ("loader") as HTMLDivElement;
           simTitle.innerHTML = "purchase succeeded!";
           const orderNumber = document.createElement("p");
+          orderNumber.id = "orderNumber"
           orderNumber.innerHTML = "Ordernumber: 123456";
           simLoad.appendChild(orderNumber);
+          console.log(cartList);
+          cartList.splice(0);
+          console.log(cartList);
     setTimeout(() => {
       simLoad.style.display = "none";
-    }, 4000)
+    }, 1000)
     loader.style.display = "none";
-  }, 4000);
+  }, 1000);
 })

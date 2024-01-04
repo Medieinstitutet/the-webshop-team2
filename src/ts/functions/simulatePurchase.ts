cfe594d43ff1537bf35ac55c0d8926d629b92198
Ali <ali.rezai27461@gmail.com>
2024-01-04 15:10:14 +0100

    :art:

--- src/ts/functions/simulatePurchase.ts
+++ src/ts/functions/simulatePurchase.ts
@@ -1,21 +1,21 @@
 import { cartList } from "../main";
 
-export function simulatePurchase () {
-const simLoad = document.getElementById("simLoad") as HTMLDivElement;
-simLoad.classList.add("showSimLoad"); 
+export function simulatePurchase() {
+  const simLoad = document.getElementById("simLoad") as HTMLDivElement;
+  simLoad.classList.add("showSimLoad");
 
-setTimeout(() => {
-  const simTitle = document.getElementById("simTitle") as HTMLDivElement;
-  const loader = document.getElementById("loader") as HTMLDivElement;
-  simTitle.innerHTML = "purchase succeeded!";
-  const orderNumber = document.createElement("p");
-  orderNumber.id = "orderNumber";
-  orderNumber.innerHTML = "Ordernumber: 123456";
-  simLoad.appendChild(orderNumber);
-  cartList.splice(0);
   setTimeout(() => {
-    simLoad.style.display = "none";
+    const simTitle = document.getElementById("simTitle") as HTMLDivElement;
+    const loader = document.getElementById("loader") as HTMLDivElement;
+    simTitle.innerHTML = "purchase succeeded!";
+    const orderNumber = document.createElement("p");
+    orderNumber.id = "orderNumber";
+    orderNumber.innerHTML = "Ordernumber: 123456";
+    simLoad.appendChild(orderNumber);
+    cartList.splice(0);
+    setTimeout(() => {
+      simLoad.style.display = "none";
+    }, 4000);
+    loader.style.display = "none";
   }, 4000);
-  loader.style.display = "none";
-}, 4000);
 }

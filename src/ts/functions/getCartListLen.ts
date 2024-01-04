510ef142f12b9dde1d441db8525b596e10da8bb4
Ali <ali.rezai27461@gmail.com>
2024-01-04 15:30:20 +0100

    :art:

--- src/ts/functions/getCartListLen.ts
+++ src/ts/functions/getCartListLen.ts
@@ -3,8 +3,10 @@ import { cartList } from "../main";
 export const getCartListLen = () => {
   const cartLen = document.getElementById("cartLen") as HTMLParagraphElement;
   if (cartList.length >= 0) {
+    let cartLenQuantity = 0;
     for (let i = 0; i < cartList.length; i++) {
-      cartLen.innerHTML = cartList.length * cartList[i].quantity + "";
+      cartLenQuantity += cartList[i].quantity - 1;
     }
+    cartLen.innerHTML = cartList.length + cartLenQuantity + "";
   }
 };
